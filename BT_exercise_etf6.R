d0551ff90e5c3cf7f20bf4413db070888c6729fc
swtzang <swtzang@gmail.com>
2020-06-08 07:50:22 +0000

    0608

--- BT_exercise_etf6.R
+++ BT_exercise_etf6.R
@@ -64,11 +64,12 @@ for (i in names(etf3)) {
   i <- paste(i, '.sma.cross', sep = '')
   model[[i]] <- bt.run(data)
 }
-
+#-------------------------------------------------
 strategy.performance.snapshoot(model, T)
 plotbt(model, plotX = T, log = 'y', LeftMargin = 3)            
 mtext('Cumulative Performance', side = 2, line = 1)
-#
+plotbt.strategy.sidebyside(model, return.table=T, make.plot = F)
+# --------------------------------------------------
 model <- list()
 #
 model$buy.hold <- bt.run(data)
@@ -128,8 +129,61 @@ strategy.performance.snapshoot(models,T)
 plotbt(models, plotX = T, log = 'y', LeftMargin = 3)            
 mtext('Cumulative Performance', side = 2, line = 1)
 
+#=============================================================
+# MVP investment strategy
+#=============================================================
+# monthly rebalance
+# covariance matrix 
+# use monthly returns to compute monthly covariance matrix
+etf3.m <- to.monthly(etf3, indexAt = 'lastof', OHLC = FALSE)
+head(etf3.m)
+etf3.w <- to.weekly(etf3, indexAt = 'lastof', OHLC = FALSE)
+head(etf3.w)
+# 
 
+#=================================================================
+# MVP portfolio
+#=================================================================
+# Reset inputs to SIT bt function
+data$prices = data$weight = data$execution.price = etf3.m
+#data$prices <- industry.price.sample
+#data$weight <- industry.price.sample
+#data$execution.price <- industry.price.sample
+data$execution.price[] <- NA
+prices <- data$prices
+n <- ncol(prices)
+#*****************************************************************
+# Create Constraints
+#*****************************************************************
+constraints = new.constraints(n, lb = -Inf, ub = +Inf)
+# SUM x.i = 1
+constraints = add.constraints(rep(1, n), 1, type = '=', constraints)        
+#
+ret = prices / mlag(prices) - 1
+weight = coredata(prices)
+weight[] = NA
+# i = 36
+for (i in 36:dim(weight)[1]) {
+  # using 36 historical monthly returns
+  hist = ret[ (i- 36 +1):i, ]
+  hist = na.omit(hist)
+  # create historical input assumptions
+  ia = create.historical.ia(hist, 12)
+  s0 = apply(coredata(hist),2, sd)     
+  # ia$cov = cov(coredata(hist))
+  ia$cov = cor(coredata(hist), use='complete.obs',method='kendall') * (s0 %*% t(s0))
+  # use min.risk.portfolio() to compute MVP weights
+  weight[i,] = min.risk.portfolio(ia, constraints)
+}
 
+# apply(weight, 1, sum)
+
+data$weight[] = weight     
+#capital = 100000
+#data$weight[] = (capital / prices) * data$weight
+model$min.var.monthly = bt.run(data)
+#
+plotbt.strategy.sidebyside(model, return.table=T, make.plot = F)
 
 
 
